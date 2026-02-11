279c322cfe3118f83100a50398ea37e59990d646
Jaffindurai29 <jaffindurai29@gmail.com>
2026-02-12 00:12:31 +0530

    feat: Add initial Next.js root layout with font imports and basic HTML structure.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Outfit, Patrick_Hand } from "next/font/google";
+import { Architects_Daughter, Geist, Geist_Mono, Inter, Manrope, Outfit, Patrick_Hand } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
